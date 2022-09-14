deaf6b1c42117e1a4c766bcb082422524617819a
Lucas Couto <lukinhacoutoo@gmail.com>
2022-09-14 14:36:33 -0300

    Alterações 12/09

--- assets/User.js
+++ assets/User.js
@@ -1,27 +1,58 @@
 class User{
 
+    KEY_USER_LOGGED = "user_logged";
+    KEY_USER_LOGGED_EMAIL = "user_logged_email";
+
     userAllowed = [
        {
-            email: "desenvolvedor.contato@gmail.com",
-            password: "2121213"
+            email: "lukinhacoutoo@gmail.com",
+            password: "123"
        },
        {
             email: "contato@gmail.com",
-            password: "2121213"
+            password: "123"
        }
     ];
     
-    login(email, password){
+    login(email, password, rememberMe){
+
+        var result = false;
+
+        var self = this;
+
         this.userAllowed.map(function(item, index){
             
-            if(item.email === email && item.password === password){
-                alert('SUCESSO')
-            }
+            if(item.email === email){
+                if(item.password === password){
 
+                    result = true;
+
+                    if(rememberMe == true){
+                        localStorage.setItem(self.KEY_USER_LOGGED, true);
+                        localStorage.setItem(self.KEY_USER_LOGGED_EMAIL, email);
+                    }
+                    else{
+                        sessionStorage.setItem(self.KEY_USER_LOGGED, true);
+                        sessionStorage.setItem(self.KEY_USER_LOGGED_EMAIL, email);
+                    }
+                    
+                }
+            }
         });
+
+        return result;
     }
 
     logout(){
 
+    }
+
+    isAuthenticated(){
+
+        var sessionStorageLogged = sessionStorage.getItem(this.KEY_USER_LOGGED);
+        var localStorageLogged = localStorage.getItem(this.KEY_USER_LOGGED_EMAIL);
+
+        return sessionStorageLogged || localStorageLogged;
+        
     }
 }
--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,21 @@
 
 var myUser = new User();
 
+function showTodoList(){
+    $(".form-signin").hide('slow');
+    $(".todo-container").show('slow');
+}
+
 $(document).ready(function(){
 
+   var resultIsAuthenticated =  myUser.isAuthenticated();
 
+   if(resultIsAuthenticated){
+        showTodoList()
+   }
+   else{
+     $(".form-signin").show('slow');
+   }
 
 
     $("#buttonSignIn").click(function(){
@@ -11,9 +23,18 @@ $(document).ready(function(){
         var emailValue = $("#inputEmail").val();
         var passwordValue = $("#inputPassword").val();
 
-        myUser.login(emailValue, passwordValue);
+        var rememberMe = $("#remember_me").is(":checked");
+        var resultLogin = myUser.login(emailValue, passwordValue, rememberMe);
 
+        
+
+        if(resultLogin == true){
+            alert('USARIO LOGADO COM SUCESSO');
+            showTodoList();
+        }
+        else{
+            alert('Usário não logado, por favor verifique seus dados');
+        }
     });    
-
 
 })
